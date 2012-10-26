2f83cd21056e3b4c5b8725d826ce052fb6b2cfa5
Cleyton Ferrari <cleyton@w7br.com>
2012-10-25 22:59:57 -0400

    Adicionado HTML, da tela de login, da tela inicial do painel, e da tela inicial do modulo administrativo;

---
+++ Projeto.Beta/UI.Web/Areas/Painel/Controllers/AdministrativoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Web.Areas.Painel.Controllers
+{
+    public class AdministrativoController : Controller
+    {
+        //
+        // GET: /Painel/Administrativo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Projeto.Beta/UI.Web/Areas/Painel/Controllers/PainelController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Web.Areas.Painel.Controllers
+{
+    public class PainelController : Controller
+    {
+        //
+        // GET: /Painel/Painel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
